6bca0c6454f1b7123b0d739d22c4947f550638fc
Eryk Holipka <79263592+Eryczal@users.noreply.github.com>
2025-02-23 14:54:21 +0100

    Update text

--- src/components/ReadyText.jsx
+++ src/components/ReadyText.jsx
@@ -2,10 +2,13 @@ import styles from "./ReadyText.module.css";
 
 function ReadyText() {
     return (
-        <div>
-            <h1>tekst</h1>
-            <div>
-                <p>tekst</p>
+        <div className={styles.container}>
+            <h1>Odkryj kosmos</h1>
+            <div className={styles.text}>
+                <p>
+                    Zwiedź kosmos z bliska, przeglądając planety znajdujące się w układzie słonecznym i nie tylko. Masz okazję zobaczyć je z bliska i poznać coś
+                    nowego na ich temat.
+                </p>
             </div>
         </div>
     );
